dde3cbc3d22b614672c025db284fbe24e0ab0ba3
emon <imrannaaziremon@gmail.com>
2022-11-14 22:04:55 +0600

    schema designed and posted data

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+  console.log(`Database successfully connected!`.green.bold);
+})
 
 // server
 const port = process.env.PORT || 8080;
